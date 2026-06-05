b9ef90ec7422e35976139d03a23cf79f212d22ac
P1er0VL <u20211a620@upc.edu.pe>
2026-06-05 04:57:11 -0500

    chore: initial Spring Boot setup para iam service

---
+++ src/test/java/pe/upc/edu/bibflipbackend/iam/BibflipIamServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.upc.edu.bibflipbackend.iam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibflipIamServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
